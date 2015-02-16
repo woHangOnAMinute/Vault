ccc9b159b3f7b89fc896cdad8364ebc6411685b9
Josh Kay <JKay@Josh-Kays-iMac-27.local>
2015-02-17 09:10:21 +1100

    yeoman build

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('vaultApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
